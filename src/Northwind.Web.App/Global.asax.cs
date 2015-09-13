d639a6e29645f2f8b1e56ed56ae1cf19dd4b7484
john kelly <jk@blue-pear.com>
2015-09-13 16:56:32 +0100

    Fix to incorrect nugets

--- src/Northwind.Web.App/Global.asax.cs
+++ src/Northwind.Web.App/Global.asax.cs
@@ -1,16 +1,14 @@
 ﻿namespace Northwind.Web.App
 {
+    using App_Start;
     using System.Web.Http;
-    using Northwind.Web.App.App_Start;
 
     public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            UnityConfig.RegisterComponents();                       
+            UnityConfig.RegisterComponents();
         }
-
-
     }
 }
